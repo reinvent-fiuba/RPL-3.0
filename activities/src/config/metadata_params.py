bcfe025ce24e18ef5bf4afa561cc0f6c974e6c31
MiguelV5 <mvasquezj@fi.uba.ar>
2024-11-10 05:11:10 -0300

    fix devcontainer configs

---
+++ activities/src/config/metadata_params.py
@@ -0,0 +1,10 @@
+FASTAPI_METADATA = {
+    "title": "RPL API",
+    "version": "3.0.0",
+    "openapi_tags": [
+        {
+            "name": "Activities",
+            "description": "Endpoints to manage activities",
+        },
+    ],
+}
