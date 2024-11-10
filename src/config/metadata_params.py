4e598b1dcd2932da2ace59e1755fc4ac32416a7d
MiguelV5 <mvasquezj@fi.uba.ar>
2024-11-10 02:39:49 -0300

    :tada: init project + devcontainers setup

---
+++ src/config/metadata_params.py
@@ -0,0 +1,10 @@
+FASTAPI_METADATA = {
+    "title": "RPL API",
+    "version": "3.0.0",
+    "openapi_tags": [
+        {
+            "name": "Users",
+            "description": "Endpoints to get, create, modify and delete users",
+        },
+    ],
+}
