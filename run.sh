2ee5ba36dc2f88bce347d3202e5ee55295b16507
MiguelV5 <mvasquezj@fi.uba.ar>
2024-12-06 04:12:00 -0300

    Move scripts to makefile; Update devcontainer to avoid collision with local compose commands; Divide pip requirements & include only the explicitly needed ones (not dumping from pip freeze for mantainability)

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source .env
-docker compose -f docker-compose.yml -f docker-compose.local.yml -f metaservices.local.yml up -d --build --remove-orphans
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-docker compose -f docker-compose.yml -f docker-compose.local.yml -f metaservices.local.yml stop
-docker compose -f docker-compose.yml -f docker-compose.local.yml -f metaservices.local.yml down
