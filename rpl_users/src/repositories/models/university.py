436c4dc12286e333b8d8ef46c287bd9365f6ea79
Luciano Martin Gamberale <lgamberale@fi.uba.ar>
2025-04-20 20:33:24 +0000

    ♻ improve how university degrees are accessed

--- rpl_users/src/repositories/models/university.py
+++ rpl_users/src/repositories/models/university.py
@@ -1,6 +1,10 @@
+from typing import List
+
 from sqlalchemy.orm import Mapped
 
 from .base_model import Base, IntPK, LargeStr, Str
+
+PERMISSION_DELIMITER = ","
 
 
 class University(Base):
@@ -9,3 +13,7 @@ class University(Base):
     id: Mapped[IntPK]
     name: Mapped[Str]
     degrees: Mapped[LargeStr]
+
+    def get_degrees(self) -> List[str]:
+        """Get the degrees as a list of strings."""
+        return [degree.strip() for degree in self.degrees.split(PERMISSION_DELIMITER)]
