05d1e42d1fb1c24b2a4b3dfa055c2f6d8d40db0d
erick <egmartinez@fi.uba.ar>
2025-05-05 16:10:21 +0000

    :construction: shedule wwekly data cleanup job

--- rpl_users/src/main.py
+++ rpl_users/src/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
@@ -6,8 +7,44 @@ from .config.api_metadata import FASTAPI_METADATA
 from .routers.users import router as users_router
 from .routers.courses import router as courses_router
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from contextlib import asynccontextmanager
+from pytz import utc
+from rpl_users.src.schedule import cleanup_validation_tokens_job
 
-app = FastAPI(**FASTAPI_METADATA)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan event handler for the FastAPI application.
+    This function is called when the application starts.
+    It initializes the APScheduler and sets up a job to run weekly.
+    """
+    scheduler = BackgroundScheduler(timezone=utc)
+    scheduler.start()
+
+    # Trigger every Saturday at 00:00
+    trigger = CronTrigger(
+        day_of_week="sat",
+        hour=0,
+        minute=0,
+        second=0,
+    )
+    # Add a job to the scheduler
+    scheduler.add_job(
+        func=cleanup_validation_tokens_job,
+        trigger=trigger,
+        id="weekly_data_cleanup",
+        replace_existing=True,
+    )
+    yield
+
+    # Gratefully shutdown the scheduler when the app is shutting down
+    scheduler.shutdown()
+
+
+app = FastAPI(**FASTAPI_METADATA, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
